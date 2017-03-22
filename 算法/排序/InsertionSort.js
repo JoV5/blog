9af075afdc73975c3cad88022ddbab6a30c686e8
JoV5 <360870202@qq.com>
2017-03-22 16:32:20 +0800

    update

--- 算法/排序/InsertionSort.js
+++ 算法/排序/InsertionSort.js
@@ -1,14 +1,17 @@
 /**
- * 插入排序（insertion sort）由N-1趟排序组成。对于P=1趟到P=N-1趟（pass），插入排序保证从位置0到位置P上的元素为已排序状态。
- * 插入排序利用了这样的事实：位置0到位置P-1上的元素是已排序过的。
+ * 插入排序（insertion sort）由len-1趟排序组成。对于i=1趟到i=len-1趟（pass），插入排序保证从位置0到位置i上的元素为已排序状态。
+ * 插入排序利用了这样的事实：位置0到位置i-1上的元素是已排序过的。
  */
-function insertionSort(arr) {
-  let N = arr.length,
-    P, j;
-  for (P = 1; P < N; P++) { // 从P = 1开始
-    let tmp = arr[P]; // 先取出位置P上的元素
+function insertionSort(arr, left, right) {
+  let i, j, tmp;
+  
+  typeof left === 'undefined' && (left = 0);
+  typeof right === 'undefined' && (right = arr.length - 1);
+  
+  for (i = left + 1; i <= right; i++) { // 从i = 1开始
+    tmp = arr[i]; // 先取出位置i上的元素
 
-    for (j = P; j > 0 && tmp < arr[j - 1]; j--) { // 选取合适的插入位置
+    for (j = i; j > left && tmp < arr[j - 1]; j--) { // 选取合适的插入位置
       arr[j] = arr[j - 1]; // 向后平移
     }
 
@@ -25,27 +28,37 @@ function insertionSort(arr) {
  * 通过交换相邻元素进行排序的任何算法平均需要Ω(N²)时间
  */
 
+
+
+/**
+ * 折半插入排序（binary insertion sort）是对插入排序算法的一种改进，由于排序算法过程中，就是不断的依次将元素插入前面已排好序的序列中。
+ * 由于前半部分为已排好序的数列，这样我们不用按顺序依次寻找插入点，可以采用折半查找的方法来加快寻找插入点的速度。
+ * 
+ * 折半插入排序算法是一种稳定的排序算法，比直接插入算法明显减少了关键字之间比较的次数，因此速度比直接插入排序算法快，
+ * 但记录移动的次数没有变，所以折半插入排序算法的时间复杂度仍然为O(n^2)，与直接插入排序算法相同。附加空间O(1)。
+ * 折半查找只是减少了比较次数，但是元素的移动次数不变，所以时间复杂度为O(n^2)是正确的！
+ */
 function insertionSort2(arr) {
-  for (let i = 1, len = arr.length; i < len; i++) {
-    let tmp = arr[i];
-    let left = 0, right = i - 1;
+  let i, j, len, tmp, left, right, middle;
+  for (i = 1, len = arr.length; i < len; i++) {
+    tmp = arr[i];
+    left = 0;
+    right = i - 1;
 
-    while (left < right) {
-      let middle = parseInt((left + right) / 2);
+    while (left <= right) {
+      middle = (left + right) >> 1;
 
-      if (arr[middle] < tmp) {
+      if (arr[middle] <= tmp) {
         left = middle + 1;
-      } else if (arr[middle] > tmp) {
-        right = middle - 1;
       } else {
-        left = right = middle;
-      }
-
-      for (let j = i; j > left; j--) {
-        arr[j] = arr[j - 1];
+        right = middle - 1;
       }
     }
-    
+
+    for (j = i; j > left; j--) {
+      arr[j] = arr[j - 1];
+    }
+
     arr[left] = tmp;
   }
 
--- 算法/排序/QuickSort.js
+++ 算法/排序/QuickSort.js
@@ -5,3 +5,93 @@
  * 3.将S - {v}（S中其余元素）分成两个不相交的集合：S1 = {x ∈ S - {v} | x ≤ v}和S2 = {x ∈ S - {v} | x ≥ v}。
  * 4.返回{quicksort(S1)后，继随v，继而quicksort(S2)} 
  */
+
+function swap(arr, i, j) {
+  let tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+}
+
+function median3(arr, left, right) {
+  let center = (left + right) >> 1;
+  
+  if (arr[left] > arr[center]) {
+    swap(arr, left, center);
+  }
+  if (arr[left] > arr[right]) {
+    swap(arr, left, right);
+  }
+  if (arr[center] > arr[right]) {
+    swap(arr, center, right);
+  }
+  
+  swap(arr, center, right - 1);
+  
+  return arr[right - 1];
+}
+
+function quickSort(array) {
+  // 交换元素位置
+  function swap(array, i, k) {
+    var temp = array[i];
+    array[i] = array[k];
+    array[k] = temp;
+  }
+  // 数组分区，左小右大
+  function partition(array, left, right) {
+    var storeIndex = left;
+    var pivot = array[right]; // 直接选最右边的元素为基准元素
+    for (var i = left; i < right; i++) {
+      if (array[i] < pivot) {
+        swap(array, storeIndex, i);
+        storeIndex++; // 交换位置后，storeIndex 自增 1，代表下一个可能要交换的位置
+      }
+    }
+    swap(array, right, storeIndex); // 将基准元素放置到最后的正确位置上
+    return storeIndex;
+  }
+  function sort(array, left, right) {
+    if (left > right) {
+      return;
+    }
+    var storeIndex = partition(array, left, right);
+    sort(array, left, storeIndex - 1);
+    sort(array, storeIndex + 1, right);
+  }
+  sort(array, 0, array.length - 1);
+  return array;
+}
+
+let  cutoff = 3;
+
+function quickSort2(arr, left, right) {
+  let i, j, pivot;
+  
+  typeof left === 'undefined' && (left = 0);
+  typeof right === 'undefined' && (right = arr.length - 1);
+  
+  if (left + cutoff <= right) {
+    pivot = median3(arr, left, right);
+    i = left;
+    j = right - 1;
+    
+    for (;;) {
+      while (arr[++i] < pivot) {}
+      while (arr[--j] > pivot) {}
+      if (i < j) {
+        swap(arr, i, j);
+      } else {
+        break;
+      }
+    }
+    
+    swap(arr, i, right - 1);
+    
+    quickSort2(arr, left, i - 1);
+    quickSort2(arr, i + 1, right);
+  } else {
+    insertionSort(arr, left, right);
+  }
+  
+  return arr;
+}
